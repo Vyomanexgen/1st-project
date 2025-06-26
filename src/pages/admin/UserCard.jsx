41b815c8df79ecb4fa7933e49bb70858de123819
sanath2567 <sriramsanath555@gmail.com>
2025-06-26 15:28:17 +0530

    updated the admin panel

--- src/pages/admin/UserCard.jsx
+++ src/pages/admin/UserCard.jsx
@@ -1,21 +1,7 @@
-// import React from 'react';
-// import styles from './UserCard.css';
-
-// const UserCard = ({ user, onSelect }) => {
-//   return (
-//     <div className={styles.userCard} onClick={() => onSelect(user)}>
-//       <img src={user.photoUrl} alt={user.name} />
-//       <p>{user.name}</p>
-//     </div>
-//   );
-// };
-
-// export default UserCard;
-
 import React from 'react';
-import styles from './UserCard.module.css'; // ✅ make sure it's .module.css
+import styles from './UserCard.module.css'; 
 
-const fallbackImage = '/images/contact-icon-illustration-isolat.png'; // replace with yours
+const fallbackImage = '/images/contact-icon-illustration-isolat.png'; 
 
 const UserCard = ({ user, onSelect }) => {
   const handleImageError = (e) => {
